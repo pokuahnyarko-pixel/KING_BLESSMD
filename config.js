28beef788d0fb7a2d387d23c5fbdbe49992c5fe3
pokuahnyarko-pixel <pokuahnyarko@gmail.com>
2026-07-26 18:24:13 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    prefix: '.',                 // command prefix
+    botName: 'KING_BLESS XMD',
+    botNumber: '',               // will be filled after first connect
+    mode: 'public',              // public (no owner filter)
+    autoTyping: true,
+    autoReact: true,
+    antiDelete: true,
+    // extra settings
+    menuImage: './assets/main-menu.jpg',
+};
