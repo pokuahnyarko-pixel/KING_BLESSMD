460249d62d62d41b83c640de49b3ea6fe20a7b32
pokuahnyarko-pixel <pokuahnyarko@gmail.com>
2026-07-26 18:14:50 +0000

    Create video.js

---
+++ commands/video.js
@@ -0,0 +1,22 @@
+const ytdl = require('ytdl-core');
+const { sendWithTyping } = require('../lib/functions');
+
+module.exports = {
+    name: 'video',
+    async execute(sock, msg, args, config) {
+        const url = args[0];
+        if (!url) return sendWithTyping(sock, msg.key.remoteJid, { text: '❌ Please provide a YouTube URL' });
+        if (!ytdl.validateURL(url)) return sendWithTyping(sock, msg.key.remoteJid, { text: '❌ Invalid YouTube URL' });
+
+        const info = await ytdl.getInfo(url);
+        const title = info.videoDetails.title;
+        await sendWithTyping(sock, msg.key.remoteJid, { text: `📹 Downloading: *${title}*...` });
+
+        const stream = ytdl(url, { filter: 'videoandaudio', quality: 'highest' });
+        await sock.sendMessage(msg.key.remoteJid, {
+            video: { stream },
+            caption: title,
+            mimetype: 'video/mp4'
+        });
+    }
+};
